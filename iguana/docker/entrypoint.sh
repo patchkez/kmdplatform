781d7464e3e5f6383701608769491cd9b47d2a35
patchkez <patchkez@gmail.com>
2018-04-22 21:34:24 +0200

    network fix

--- iguana/docker/entrypoint.sh
+++ iguana/docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Tempatize bitcoin and komodo configuration files
 confd -confdir ~/confd -onetime -backend env
 
@@ -11,10 +10,10 @@ function templatize_acs {
   else
     BRANCH="production"
   fi
- 
+
   export LC_ALL=C.UTF-8
   export LANG=C.UTF-8
-  source ~/komodotools-venv/bin/activate 
+  source ~/komodotools-venv/bin/activate
   cd ~/komodotools/dragonriders
   dokomodo generate_assetchains_conf -b ${BRANCH}
   cd -
@@ -22,23 +21,30 @@ function templatize_acs {
 
 
 # If DEBUG var is not empty, debugging is enabled
-if [ -n "${DEBUG}" ]; then 
+if [ -n "${DEBUG}" ]; then
   DEBUG_PREFIX="gdb -args"
 else
   DEBUG_PREFIX=""
 fi
 
 cd ~/SuperNET/iguana
-# Are we running in TEST/DEV mode?
-if [ "${IGUANA_BRANCH}" = "dev" -o "${IGUANA_BRANCH}" = "jl777" ];then
-  templatize_acs ${IGUANA_BRANCH}
-  # Download latest testnet file wiht btcpubkeys
-  curl -o testnet https://raw.githubusercontent.com/KomodoPlatform/vote2018/master/testnet/testnet.json
-  exec ${DEBUG_PREFIX} ~/SuperNET/agents/iguana testnet
 
-else 
-  templatize_acs ${IGUANA_BRANCH}
-  exec ${DEBUG_PREFIX} ~/SuperNET/agents/iguana "${args[@]}"
+# Check if IGUANA_BRANCH was passed as env variable
+if [ -z ${IGUANA_BRANCH} ];then
+  echo "IGUANA_BRANCH variable not set!!!"
+  exit 1
 fi
 
-# exec "$@"
+# Are we running in TEST/DEV mode?
+if [ "${IGUANA_BRANCH}" = "beta" ];then
+  iguana_mode="notary"
+elif [ "${IGUANA_BRANCH}" = "dev" -o "${IGUANA_BRANCH}" = "jl777" ];then
+  iguana_mode="testnet"
+  curl -o testnet https://raw.githubusercontent.com/KomodoPlatform/vote2018/master/testnet/testnet.json
+else 
+  echo "Unknown branch variable passed..."
+fi
+
+
+templatize_acs ${IGUANA_BRANCH}
+exec ${DEBUG_PREFIX} ~/SuperNET/agents/iguana ${iguana_mode}
